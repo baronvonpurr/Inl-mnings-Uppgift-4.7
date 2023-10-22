838d1d70ed273570228eddd63f10bbe2164f0ac7
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-22 16:50:55 +0200

    Add project files.

---
+++ Inlämnings Uppgift 4.7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal");
+            string svar = (Console.ReadLine());
+            Console.WriteLine("Hur många steg ska talet flyttas?");
+            int steg = int.Parse(Console.ReadLine());
+            for (int position = 0; position < svar.Length; position++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(svar[position]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
